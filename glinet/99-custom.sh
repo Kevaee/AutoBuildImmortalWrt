1881f1062f0253cca3c330683bfa0e3bff0c5656
晚风 <115437768+Kevaee@users.noreply.github.com>
2026-06-21 20:02:23 +0800

    Update 99-custom.sh

--- glinet/99-custom.sh
+++ glinet/99-custom.sh
@@ -101,8 +101,8 @@ uci set dropbear.@dropbear[0].Interface=''
 uci commit
 
 #设置路由器主机名
-uci set system.@system[0].hostname="MT3000" 
-uci commit system
+#uci set system.@system[0].hostname="MT3000" 
+#uci commit system
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
